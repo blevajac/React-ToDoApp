cb0b44d1164b5a7bd1f74c7157e6334c2463a823
Boris <blevajac@gmail.com>
2017-02-03 14:22:26 +0100

    Dodana staićak todo lista

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,40 @@
 var React = require('react');
+//komponente
+var TodoList = require('TodoList');
+
 
 var TodoApp = React.createClass({
+  getInitialState: function(){      //vraća default stanje applikacije
+    return{
+          todos: [
+            {
+                id: 1,
+                text: 'Prošeći zeca'
+            }, {
+                id: 2,
+                text: 'Posaugaj kuću'
+            }, {
+                id: 3,
+                text: 'Odi do dućana'
+            }, {
+                id: 4,
+                text: 'Nazovi svoje'
+            }, {
+                id: 5,
+                text: 'Jasmina zamolba'
+            }
+
+          ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;   //dohvaća state todos
+
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/> {/*renderira TodoList komponentu */}
       </div>
-    )
+    );
   }
 });
 
