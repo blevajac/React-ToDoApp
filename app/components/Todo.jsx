eabc440e817d71db8045136bd288630db1edc7a0
bleavjac <b.levajac@gmail.com>
2017-03-31 16:32:21 +0200

    Firevase SAVE newTODO

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -25,7 +25,7 @@ export var Todo = React.createClass({
           return(
               <div className={todoClassName} onClick = {() => {           //click handler
                       //this.props.onToggle(id);  //kada se stisne checkbox ili text on se bude pokrenuo
-                      dispatch(actions.toggleTodo(id));
+                      dispatch(actions.startToggleTodo(id, !completed));
               }}>
                   <div>
                       <input type="checkbox" checked={completed} />
--- app/redux/reducers/reducers.jsx
+++ app/redux/reducers/reducers.jsx
@@ -27,16 +27,13 @@ export var todosReducer = (state = [], action) => {
                 ...state,
                 action.todo
             ];
-        case 'TOGGLE_TODO':
+        case 'UPDATE_TODO':
             return state.map((todo) => {
                 if(todo.id === action.id) {
-                    var nextCompleted = !todo.completed;
-
                     return {
                         ...todo,
-                        completed: nextCompleted,
-                        completedAt: nextCompleted ? moment().unix() : undefined
-                    };
+                        ...action.updates   //drugi spread operator (...) briše sve iste elemente unutar prvog (ali ostavlja ne iste)
+                    }                
                 }else {
                   return todo;
                 }
